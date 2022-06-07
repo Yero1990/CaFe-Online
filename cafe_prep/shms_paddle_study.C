60a56d22559f2f93a9c783b076f7191fc13f01bf
Carlos Yero <cyero@jlab.org>
2022-06-06 23:19:39 -0400

    updated

--- cafe_prep/shms_paddle_study.C
+++ cafe_prep/shms_paddle_study.C
@@ -6,50 +6,49 @@ void shms_paddle_study(){
 
   TH1F *hcoin_time=0;
 
-  TCanvas *c1 = new TCanvas("c1", "", 500,500);
+  //TCanvas *c1 = new TCanvas("c1", "", 500,500);
   TCanvas *c2 = new TCanvas("c2", "", 500,500);
-  TCanvas *c3 = new TCanvas("c3", "", 500,500);
-  TCanvas *c4 = new TCanvas("c4", "", 500,500);
-  TCanvas *c5 = new TCanvas("c5", "", 500,500);
+  //TCanvas *c3 = new TCanvas("c3", "", 500,500);
+  //TCanvas *c4 = new TCanvas("c4", "", 500,500);
+  //TCanvas *c5 = new TCanvas("c5", "", 500,500);
   
-  c1->cd();
-  T->Draw("CTime.epCoinTime_ROC2>>hcoin_time(100,0,400)");
+  //c1->cd();
+  //T->Draw("CTime.epCoinTime_ROC2>>hcoin_time(100,-100,400)");
+
 
   c2->cd();
   c2->Divide(2,2);
   c2->cd(1);
   // S1X+ occupancy
-  T->Draw("P.hod.1x.numGoodPosAdcHits>>noCuts(100,0.01,14)", "");
-  T->Draw("P.hod.1x.numGoodPosAdcHits>>delta>=5(100,0.01,14)", "P.gtr.dp>=5", "sames");
-  T->Draw("P.hod.1x.numGoodPosAdcHits>>ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5", "sames");
-
+  T->Draw("P.hod.1x.numGoodPosAdcHits>>noCuts(100,0.01,14)", "");  
+  T->Draw("P.hod.1x.numGoodPosAdcHits>>delta>=5(100,0.01,14)", "P.gtr.dp>=5", "sames");  
+  T->Draw("P.hod.1x.numGoodPosAdcHits>>ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5", "sames"); 
+ 
+  
   c2->cd(2);
   // S1X- occupancy
-  T->Draw("P.hod.1x.numGoodNegAdcHits>>S1X-_noCuts(100,0.01,14)", "");
-  T->Draw("P.hod.1x.numGoodNegAdcHits>>S1X-_ctime(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
-  T->Draw("P.hod.1x.numGoodNegAdcHits>>S1X-_ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5", "sames");
-
+  T->Draw("P.hod.1x.numGoodNegAdcHits>>noCuts(100,0.01,14)", "");
+  T->Draw("P.hod.1x.numGoodNegAdcHits>>delta>=5(100,0.01,14)", "P.gtr.dp>=5", "sames");
+  T->Draw("P.hod.1x.numGoodNegAdcHits>>ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5", "sames");
   c2->cd(3);
   // S2X+ occupancy
-  T->Draw("P.hod.2x.numGoodPosAdcHits>>S2X+_noCuts(100,0.01,14)", "");
-  T->Draw("P.hod.2x.numGoodPosAdcHits>>S2X+_ctime(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
-  T->Draw("P.hod.2x.numGoodPosAdcHits>>S2X+_ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5", "sames");
-
+  T->Draw("P.hod.2x.numGoodPosAdcHits>>noCuts(100,0.01,14)", "");
+  T->Draw("P.hod.2x.numGoodPosAdcHits>>delta>=5(100,0.01,14)", "P.gtr.dp>=5", "sames");
+  T->Draw("P.hod.2x.numGoodPosAdcHits>>ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5", "sames");
   c2->cd(4);
   // S2X- occupancy
-  T->Draw("P.hod.2x.numGoodNegAdcHits>>S2X-_noCuts(100,0.01,14)", "");
-  T->Draw("P.hod.2x.numGoodNegAdcHits>>S2X-_ctime(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
-  T->Draw("P.hod.2x.numGoodNegAdcHits>>S2X-_ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5", "sames");
-
- 
+  T->Draw("P.hod.2x.numGoodNegAdcHits>>noCuts(100,0.01,14)", "");
+  T->Draw("P.hod.2x.numGoodNegAdcHits>>delta>=5(100,0.01,14)", "P.gtr.dp>=5", "sames");
+  T->Draw("P.hod.2x.numGoodNegAdcHits>>ctime.AND.delta>=5(100,0.01,14)", "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5", "sames");
+  /*
   c3->cd();
   // shms delta (with / without occupancy cuts)
   T->Draw("P.gtr.dp>>delta_noCuts(100,-10,22)", "");
-  T->Draw("P.gtr.dp>>delta_S1X+S2X_GoodadcCounter>=7(100,-10,22)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "sames");
-  T->Draw("P.gtr.dp>>delta_S1X+S2X_adcCounter>=7(100,-10,22)", "P.hod.1x.posAdcCounter>=7&&P.hod.1x.negAdcCounter>=7&&P.hod.2x.posAdcCounter>=7&&P.hod.2x.negAdcCounter>=7", "sames");
-  T->Draw("P.gtr.dp>>delta_S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
+  //T->Draw("P.gtr.dp>>delta_S1X+S2X_GoodadcCounter>=7(100,-10,22)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "sames");
+  //T->Draw("P.gtr.dp>>delta_S1X+S2X_adcCounter>=7(100,-10,22)", "P.hod.1x.posAdcCounter>=7&&P.hod.1x.negAdcCounter>=7&&P.hod.2x.posAdcCounter>=7&&P.hod.2x.negAdcCounter>=7", "sames");
+  //T->Draw("P.gtr.dp>>delta_S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
 
-  /*
+  
   c4->cd();
   c4->Divide(3,2);
   // Hodo Track X vs. delta (track x is in dispersive direction, i.e., vertical bend)
@@ -58,21 +57,21 @@ void shms_paddle_study(){
   c4->cd(2);
   T->Draw("P.hod.1x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "colz");
   c4->cd(3);
-  T->Draw("P.hod.1x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "colz");
+  T->Draw("P.hod.1x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.", "colz");
   c4->cd(4);
   T->Draw("P.hod.2x.TrackXPos:P.gtr.dp>>noCut(100,-10,22, 100,-70,70)", "", "colz");
   c4->cd(5);
   T->Draw("P.hod.2x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "colz");
   c4->cd(6);
-  T->Draw("P.hod.2x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "colzs");
-  */
+  T->Draw("P.hod.2x.TrackXPos:P.gtr.dp>>S1X+S2X_adcCounter>=7.AND.ctime(100,-10,22, 100,-70,70)", "P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7&&CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.", "colzs");
+  
   
   c5->cd();
   //T->Draw("P.kin.primary.W>>noCut(40,0.85,1.05)", "", "");
   //T->Draw("P.kin.primary.W>>ctime(40,0.85,1.05)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.", "sames");
-  //  T->Draw("P.kin.primary.W>>ctime.AND.delta>=5(40,0.85,1.05)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5", "sames");
-  //T->Draw("P.kin.primary.W>>a(40,0.85,1.05)", "CTime.epCoinTime_ROC2>0&&CTime.epCoinTime_ROC2<20.&&P.gtr.dp>=5&&P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "sames");
-
+  T->Draw("P.kin.primary.W>>ctime.AND.delta>=5(40,0.85,1.05)",                "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5", "");
+  T->Draw("P.kin.primary.W>>ctime.AND.delta>=5.AND.S1X+S2X>=7(40,0.85,1.05)", "CTime.epCoinTime_ROC2>=0&&CTime.epCoinTime_ROC2<=20.&&P.gtr.dp>=5&&P.hod.1x.numGoodPosAdcHits>=7&&P.hod.1x.numGoodNegAdcHits>=7&&P.hod.2x.numGoodPosAdcHits>=7&&P.hod.2x.numGoodNegAdcHits>=7", "sames");
+  */
   
 }
 /*
